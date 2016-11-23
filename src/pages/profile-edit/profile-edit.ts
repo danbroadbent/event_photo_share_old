e667ff81a57320c74e9d1dd6ab64f51f93189b26
Dan Broadbent <dbroadbent85@gmail.com>
2016-11-23 11:13:16 -0700

    Add edit profile from auth project and add logout button

--- src/pages/profile-edit/profile-edit.ts
+++ src/pages/profile-edit/profile-edit.ts
@@ -1,22 +1,106 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
+import { ProfileData } from '../../providers/profile-data';
+import { AuthData } from '../../providers/auth-data';
+import { LoginPage } from '../login/login';
 
-/*
-  Generated class for the ProfileEdit page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-profile-edit',
   templateUrl: 'profile-edit.html'
 })
 export class ProfileEditPage {
+  public userProfile: any;
 
-  constructor(public navCtrl: NavController) {}
+  constructor(public nav: NavController, public profileData: ProfileData,
+    public authData: AuthData, public alertCtrl: AlertController) {
+    this.nav = nav;
+    this.profileData = profileData;
 
-  ionViewDidLoad() {
-    console.log('Hello ProfileEditPage Page');
+    this.profileData.getUserProfile().on('value', (data) => {
+      this.userProfile = data.val();
+    });
   }
 
+  logOut(){
+      this.authData.logoutUser().then(() => {
+        this.nav.setRoot(LoginPage);
+      });
+    }
+
+  updateName(){
+    let alert = this.alertCtrl.create({
+      message: "Your first name & last name",
+      inputs: [
+        {
+          name: 'firstName',
+          placeholder: 'Your first name',
+          value: this.userProfile.firstName
+        },
+        {
+          name: 'lastName',
+          placeholder: 'Your last name',
+          value: this.userProfile.lastName
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            this.profileData.updateName(data.firstName, data.lastName);
+            }
+          }
+        ]
+       });
+      alert.present();
+     }
+
+    updateEmail(){
+      let alert = this.alertCtrl.create({
+        inputs: [
+          {
+            name: 'newEmail',
+            placeholder: 'Your new email',
+          },
+        ],
+        buttons: [
+          {
+            text: 'Cancel',
+          },
+          {
+            text: 'Save',
+            handler: data => {
+              this.profileData.updateEmail(data.newEmail);
+            }
+          }
+        ]
+      });
+      alert.present();
+    }
+
+  updatePassword(){
+    let alert = this.alertCtrl.create({
+      inputs: [
+        {
+          name: 'newPassword',
+          placeholder: 'Your new password',
+          type: 'password'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            this.profileData.updatePassword(data.newPassword);
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
 }
